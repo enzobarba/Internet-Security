6a86017503537a2574ae01356fafd0f207b4b72c
enzobarba <enzo13617@gmail.com>
2025-09-13 12:52:24 +0200

    Delete Progetto directory

--- Progetto/assets/php/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$connessione = new mysqli('localhost','root','','progetto_IS');
-
-if($connessione == false){
-    die("Errore durante la connessione:". $connessione->conncet_error);
-}
-
-?>
